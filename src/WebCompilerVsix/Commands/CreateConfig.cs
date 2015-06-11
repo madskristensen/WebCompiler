a9d3fea009497a98a86570e46cd801a3e4f1b576
Mads Kristensen <post@madskristensen.net>
2015-06-11 15:16:28 -0700

    Added progress bar to config creation

--- src/WebCompilerVsix/Commands/CreateConfig.cs
+++ src/WebCompilerVsix/Commands/CreateConfig.cs
@@ -104,6 +104,7 @@ namespace WebCompilerVsix.Commands
             }
 
             // Create new config
+            WebCompilerPackage._dte.StatusBar.Progress(true, "Compiling file", 0, 3);
             string outputFile = GetOutputFileName(folder, Path.GetFileName(relativeFile));
 
             if (string.IsNullOrEmpty(outputFile))
@@ -112,12 +113,19 @@ namespace WebCompilerVsix.Commands
             string relativeOutputFile = MakeRelative(configFile, outputFile);
             Config config = CreateConfigFile(relativeFile, relativeOutputFile);
 
+            WebCompilerPackage._dte.StatusBar.Progress(true, "Compiling file", 1, 3);
+
             ConfigHandler handler = new ConfigHandler();
             handler.AddConfig(configFile, config);
 
             ProjectHelpers.AddFileToProject(item.ContainingProject, configFile, "None");
+            WebCompilerPackage._dte.StatusBar.Progress(true, "Compiling file", 2, 3);
+
             WebCompilerPackage._dte.ItemOperations.OpenFile(configFile);
+            WebCompilerPackage._dte.StatusBar.Progress(true, "Compiling file", 3, 3);
+
             CompilerService.Process(configFile);
+            WebCompilerPackage._dte.StatusBar.Progress(false, "Compiling file");
         }
 
         private static Config CreateConfigFile(string inputfile, string outputFile)
