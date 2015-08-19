b15a943a9b4748232322a12c9f50876252656563
Mads Kristensen <post@madskristensen.net>
2015-08-19 08:48:11 -0700

    LESS: Adjust relative paths by default

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@
 - [x] Optimize compilation of newly added configs (#72)
 - [x] Use `node-sass` to compile Sass files (#74)
 - [x] Source Maps support for Sass compiler
+- [x] LESS: Adjust relative paths in @import files by default
 - [ ] Generate gulpfile.js from compilerconfig.json (#34)
 - [ ] Preview window (#6)
 - [ ] File globbing pattern support (#49)
--- src/WebCompiler/Compile/LessOptions.cs
+++ src/WebCompiler/Compile/LessOptions.cs
@@ -4,21 +4,24 @@ namespace WebCompiler
 {
     class LessOptions : BaseOptions
     {
-		private const string trueStr = "true";
+        private const string trueStr = "true";
 
         public LessOptions(Config config)
         {
             StrictMath = GetValue(config, "strictMath").ToLowerInvariant() == trueStr;
             StrictUnits = GetValue(config, "strictUnits").ToLowerInvariant() == trueStr;
-            RelativeUrls = GetValue(config, "relativeUrls").ToLowerInvariant() == trueStr;
             RootPath = GetValue(config, "rootPath");
+
+            string relativeUrls = GetValue(config, "relativeUrls").ToLowerInvariant();
+            if (relativeUrls == "false")
+                RelativeUrls = false;
         }
 
         public bool StrictMath { get; set; }
 
         public bool StrictUnits { get; set; }
 
-        public bool RelativeUrls { get; set; }
+        public bool RelativeUrls { get; set; } = true;
 
         public string RootPath { get; set; }
     }
