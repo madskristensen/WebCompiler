9d78e3947cb66dfcae8083f967295c4f11f0f431
Mads Kristensen <post@madskristensen.net>
2016-11-25 13:30:59 -0800

    VS2017 support

--- src/WebCompilerVsix/JSON/CompilerConfigSchemaSelector.cs
+++ src/WebCompilerVsix/JSON/CompilerConfigSchemaSelector.cs
@@ -1,42 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.JSON.Core.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.Composition;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Threading.Tasks;
+//using Microsoft.JSON.Core.Schema;
 
-namespace WebCompilerVsix.JSON
-{
-    [Export(typeof(IJSONSchemaSelector))]
-    class CompilerConfigSchemaSelector : IJSONSchemaSelector
-    {
-        public event EventHandler AvailableSchemasChanged { add { } remove { } }
+//namespace WebCompilerVsix.JSON
+//{
+//    [Export(typeof(IJSONSchemaSelector))]
+//    class CompilerConfigSchemaSelector : IJSONSchemaSelector
+//    {
+//        public event EventHandler AvailableSchemasChanged { add { } remove { } }
 
-        public Task<IEnumerable<string>> GetAvailableSchemasAsync()
-        {
-            return Task.FromResult(Enumerable.Empty<string>());
-        }
+//        public Task<IEnumerable<string>> GetAvailableSchemasAsync()
+//        {
+//            return Task.FromResult(Enumerable.Empty<string>());
+//        }
 
-        public string GetSchemaFor(string fileLocation)
-        {
-            string fileName = Path.GetFileName(fileLocation);
+//        public string GetSchemaFor(string fileLocation)
+//        {
+//            string fileName = Path.GetFileName(fileLocation);
 
-            if (fileName.Equals(Constants.CONFIG_FILENAME, StringComparison.OrdinalIgnoreCase))
-                return GetSchemaFileName("json\\compilerconfig-schema.json");
+//            if (fileName.Equals(Constants.CONFIG_FILENAME, StringComparison.OrdinalIgnoreCase))
+//                return GetSchemaFileName("json\\compilerconfig-schema.json");
 
-            if (fileName.Equals(Constants.DEFAULTS_FILENAME, StringComparison.OrdinalIgnoreCase))
-                return GetSchemaFileName("json\\compilerdefaults-schema.json");
+//            if (fileName.Equals(Constants.DEFAULTS_FILENAME, StringComparison.OrdinalIgnoreCase))
+//                return GetSchemaFileName("json\\compilerdefaults-schema.json");
 
-            return null;
-        }
+//            return null;
+//        }
 
-        private static string GetSchemaFileName(string relativePath)
-        {
-            string assembly = Assembly.GetExecutingAssembly().Location;
-            string folder = Path.GetDirectoryName(assembly);
-            return Path.Combine(folder, relativePath);
-        }
-    }
-}
+//        private static string GetSchemaFileName(string relativePath)
+//        {
+//            string assembly = Assembly.GetExecutingAssembly().Location;
+//            string folder = Path.GetDirectoryName(assembly);
+//            return Path.Combine(folder, relativePath);
+//        }
+//    }
+//}
--- src/WebCompilerVsix/JSON/RelativeFilePathFormatProvider.cs
+++ src/WebCompilerVsix/JSON/RelativeFilePathFormatProvider.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.IO;
-using Microsoft.JSON.Core.Parser.TreeItems;
-using Microsoft.JSON.Core.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.Composition;
+//using System.IO;
+//using Microsoft.JSON.Core.Parser.TreeItems;
+//using Microsoft.JSON.Core.Schema;
 
-namespace WebCompilerVsix.JSON
-{
-    [Export(typeof(IJSONSchemaFormatHandler))]
-    class RelativeFilePathFormatProvider : JSONSchemaFormatHandlerBase
-    {
-        public override string FormatName
-        {
-            get { return "compiler_relativepath"; }
-        }
+//namespace WebCompilerVsix.JSON
+//{
+//    [Export(typeof(IJSONSchemaFormatHandler))]
+//    class RelativeFilePathFormatProvider : JSONSchemaFormatHandlerBase
+//    {
+//        public override string FormatName
+//        {
+//            get { return "compiler_relativepath"; }
+//        }
 
-        public override bool HideFormatNameCompletion
-        {
-            get { return true; }
-        }
+//        public override bool HideFormatNameCompletion
+//        {
+//            get { return true; }
+//        }
 
-        public override IEnumerable<string> GetIssues(JSONDocument doc, string canonicalizedValue)
-        {
-            if (string.IsNullOrEmpty(doc.DocumentLocation))
-                yield break;
+//        public override IEnumerable<string> GetIssues(JSONDocument doc, string canonicalizedValue)
+//        {
+//            if (string.IsNullOrEmpty(doc.DocumentLocation))
+//                yield break;
 
-            string fileName = Path.GetFileName(doc.DocumentLocation);
+//            string fileName = Path.GetFileName(doc.DocumentLocation);
 
-            if (!fileName.Equals(Constants.CONFIG_FILENAME, StringComparison.OrdinalIgnoreCase))
-                yield break;
+//            if (!fileName.Equals(Constants.CONFIG_FILENAME, StringComparison.OrdinalIgnoreCase))
+//                yield break;
 
-            string folder = Path.GetDirectoryName(doc.DocumentLocation);
-            string absolutePath = Path.Combine(folder, canonicalizedValue);
+//            string folder = Path.GetDirectoryName(doc.DocumentLocation);
+//            string absolutePath = Path.Combine(folder, canonicalizedValue);
 
-            if (!File.Exists(absolutePath) && !Directory.Exists(absolutePath))
-                yield return $"The file '{canonicalizedValue}' does not exist";
-        }
-    }
-}
+//            if (!File.Exists(absolutePath) && !Directory.Exists(absolutePath))
+//                yield return $"The file '{canonicalizedValue}' does not exist";
+//        }
+//    }
+//}
