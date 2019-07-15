194200c7733053f9692cfac22085cf6bc435c625
John Seaman <jws305@gmail.com>
2019-07-15 13:38:29 -0400

    Keep track of which Less dependencies have been checked. (#426)
    * Keep track of which Less dependencies have been checked.
    
    * Fixed Less circular dependency check.

--- src/WebCompilerTest/Compile/LessTest.cs
+++ src/WebCompilerTest/Compile/LessTest.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebCompiler;
@@ -25,6 +26,8 @@ namespace WebCompilerTest
             File.Delete("../../artifacts/less/error.css");
             File.Delete("../../artifacts/less/relative.css");
             File.Delete("../../artifacts/less/relative.min.css");
+            File.Delete("../../artifacts/less/circrefa.css");
+            File.Delete("../../artifacts/less/circrefa.min.css");
         }
 
         [TestMethod, TestCategory("LESS")]
@@ -92,6 +95,19 @@ namespace WebCompilerTest
         }
 
         [TestMethod, TestCategory("LESS")]
+        public void CompileCircularReference()
+        {
+            // Set the last write time and create outputs in a way that Config.CheckForNewerDependenciesRecursively will be called
+            File.SetLastWriteTimeUtc("../../artifacts/less/circrefa.less", DateTime.UtcNow);
+            File.SetLastWriteTimeUtc("../../artifacts/less/circrefb.less", DateTime.UtcNow);
+            File.WriteAllText("../../artifacts/less/circrefa.css", string.Empty);
+            File.WriteAllText("../../artifacts/less/circrefa.min.css", string.Empty);
+
+            // Since the outputs were generated after the inputs, no compilation should have occurred
+            var result = _processor.Process("../../artifacts/lessconfigCircRef.json");
+            Assert.AreEqual(0, result.Count<CompilerResult>());
+        }
+
         public void CompileLessLegacyStrictMath()
         {
             var result = _processor.Process("../../artifacts/lessconfigLegacyStrictMath.json");
