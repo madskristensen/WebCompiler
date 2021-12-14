44a2c807e79cba4d57168294d9a079639ed0b9ce
garcia-alexandre-pro <garcia-alexandre-pro@users.noreply.github.com>
2021-12-14 20:36:35 +0100

    VS 2022 compatibility
    splitted the WebCompilerVsix project in two distinct projects, one for VS 2022, and the other for the earlier versions (VS 2019 and earlier)
    https://docs.microsoft.com/en-us/visualstudio/extensibility/migration/update-visual-studio-extension?view=vs-2022#modernize-your-vsix-project

---
+++ src/WebCompilerVsix2022/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using WebCompilerVsix;
+
+[assembly: AssemblyTitle(Constants.VSIX_NAME)]
+[assembly: AssemblyDescription("Compiles LESS, Sass and CoffeeScript files directly within Visual Studio. This plugin is based on Mads Kristensen WebCompiler (https://github.com/madskristensen/WebCompiler) but uses dart-sass instead of node-sass.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(Constants.VSIX_NAME)]
+[assembly: AssemblyCopyright("Alex Garcia")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion(WebCompilerPackage.Version)]
+[assembly: AssemblyFileVersion(WebCompilerPackage.Version)]
